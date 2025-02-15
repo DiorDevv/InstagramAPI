cb42872a366bd02d47a86b1ea439ca2944b030c0
diyorbek <diyorbekaxmadjonov98@gamil.com>
2025-02-15 15:08:02 +0500

    Updare delety qilindi

--- post/urls.py
+++ post/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PostListView, PostCreateView
+from .views import PostListView, PostCreateView, PostRetrieveUpdateDestroyView
 
 urlpatterns = [
     path('posts/', PostListView.as_view(), name='post-list'),
     path('posts/create/', PostCreateView.as_view(), name='post-create'),
+    path('posts/<uuid:pk>/', PostRetrieveUpdateDestroyView.as_view(), name='post-create-update'),
 ]
--- post/views.py
+++ post/views.py
@@ -1,7 +1,8 @@
 from unittest.async_case import IsolatedAsyncioTestCase
+from rest_framework.response import Response
 
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, AllowAny
 
@@ -27,10 +28,33 @@ class PostCreateView(CreateAPIView):
         serializer.save(author=self.request.user)
 
 
-# class PostRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = Post.objects.all()
-#     serializer_class = PostSerializer
-#     permission_classes = [IsAuthenticated, ]
-#
-#     def get_queryset(self):
-#         return Post.objects.all()
+class PostRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, ]
+
+    def put(self, request, *args, **kwargs):
+        post = self.get_object()
+        serializer = self.serializer_class(post, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(
+            {
+                "success": True,
+                "code": status.HTTP_200_OK,
+                "message": "Post successfully updated",
+                "data": serializer.data
+            }
+        )
+
+    def delete(self, request, *args, **kwargs):
+        post = self.get_object()
+        post.delete()
+        return Response(
+            {
+                "success": True,
+                "code": status.HTTP_204_NO_CONTENT,
+                "message": "Post successfully deleted",
+
+            }
+        )
